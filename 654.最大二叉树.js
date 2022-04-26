79f2da56864865b07ac3b3bf81aa1bbb9e3d50f2
LiuYang <2472104875@qq.cpm>
2022-04-26 15:54:28 +0800

    补充 第654题 注释

--- 654.最大二叉树.js
+++ 654.最大二叉树.js
@@ -18,14 +18,25 @@
  * @return {TreeNode}
  */
 var constructMaximumBinaryTree = function (nums) {
+    // 递归退出条件
     if (nums.length === 0) return null;
+
+    // 在数组中找出最大值
     const max = Math.max(...nums);
+    // 找出最大值的索引
     const maxIndex = nums.indexOf(max);
+    // 根据最大值索引分割出 左子树数组 和 右子树数组
     const leftArr = nums.slice(0, maxIndex);
     const rightArr = nums.slice(maxIndex + 1);
+
+    // 构建节点
+    // 当前节点值为当前数组中最大值
     const root = new TreeNode(max)
+    // 当前节点的左右子树 为 左右数组构建的结点
     root.left = constructMaximumBinaryTree(leftArr);
     root.right = constructMaximumBinaryTree(rightArr);
+
+    // 返回创建的结点
     return root;
 };
 // @lc code=end
