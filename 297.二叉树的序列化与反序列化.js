a87f58991103fd90d12603da0b5ae59b9d71b8e0
LiuYang <2472104875@qq.cpm>
2022-04-28 16:49:45 +0800

    补充第297题注释

--- 297.二叉树的序列化与反序列化.js
+++ 297.二叉树的序列化与反序列化.js
@@ -20,14 +20,22 @@
  * @return {string}
  */
 var serialize = function (root) {
+    // 使用数组存放每个节点
     let res = [];
+    // 迭代遍历
     const traverse = root => {
+        // 遍历退出
         if (root === null) return res.push('null');
+        // 先序遍历，先放入根节点值
         res.push(root.val)
+        // 迭代左节点
         traverse(root.left);
+        // 迭代右节点
         traverse(root.right);
     }
     traverse(root);
+
+    // 将所有节点 先序排列 组成的数组使用 ',' 连接为字符串并返回
     return res.join(',');
 };
 
@@ -38,16 +46,25 @@ var serialize = function (root) {
  * @return {TreeNode}
  */
 var deserialize = function (data) {
-    // "1,2,#,4,#,#,3,#,#,";
+    // 将序列化后的字符串分割为数组
     const TreeArr = data.split(',');
+    // 迭代，生成树
     const traverse = TreeArr => {
+        // 由于是先序排列的，所以第一个元素为根节点
+        // 出队
         const val = TreeArr.shift();
-        const root = new TreeNode(val);
+        // 若节点为空，退出迭代
         if (val === 'null') return null;
+        // 生成树，值为出队的值
+        const root = new TreeNode(val);
+        // 由于是先序排列的，左节点为根节点后一个元素，即当前队列的第一位，进行迭代左子树
         root.left = traverse(TreeArr);
+        // 左树遍历完毕，当前队列第一位为右子树，进行迭代右子树
         root.right = traverse(TreeArr);
+        // 返回节点
         return root;
     }
+
     return traverse(TreeArr);
 };
 
